c57b94bdc361403338da352c80de53d842dce392
iminders <liuwen.w@qq.com>
2022-11-10 15:18:25 +0800

    build

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,10 +2,13 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    my_camera.reset(new QCamera(QMediaDevices::defaultVideoInput())); //新建并设置摄像头使用默认驱动
+    my_captureSession.setCamera(my_camera.data());                    //捕获摄像头画面
+    my_captureSession.setVideoOutput(ui->viewfinder);                 //设置捕捉画面显示窗口
+    my_camera->start();                                               //启动摄像头
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +16,11 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//检测摄像头驱动能否识别
+bool MainWindow::checkCameraAvailability()
+{
+    if (QMediaDevices::videoInputs().count() > 0)
+        return true; //检测到摄像头驱动
+    else
+        return false; //没有检测到摄像头驱动
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,10 +1,19 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QCamera>
+#include <QImageCapture>
 #include <QMainWindow>
+#include <QMediaCaptureSession>
+#include <QMediaDevices>
+#include <QMediaRecorder>
+#include <QVideoWidget>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+    class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,5 +26,11 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    QScopedPointer<QCamera> my_camera;
+    QScopedPointer<QMediaRecorder> my_mediaRecorder;
+    QMediaCaptureSession my_captureSession;
+
+    bool checkCameraAvailability(void);
 };
 #endif // MAINWINDOW_H
